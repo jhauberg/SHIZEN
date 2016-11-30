cebeb1e4e2a6b98910e5df7ddc55879d0270af15
Jacob Hauberg Hansen <jacob.hauberg@gmail.com>
2016-11-30 20:13:34 +0100

    Add basic graphics structures

--- src/internal.h
+++ src/internal.h
@@ -15,7 +15,19 @@
 #define GLFW_INCLUDE_NONE
 
 #include <GL/gl3w.h>
+
+#if defined(__clang__)
+ #pragma clang diagnostic push
+ #pragma clang diagnostic ignored "-Wdocumentation"
+#endif
+
 #include <GLFW/glfw3.h>
+
+#if defined(__clang__)
+ #pragma clang pop
+#endif
+
+#include "type.h"
 
 typedef struct {
     bool is_initialized;
@@ -25,5 +37,16 @@ typedef struct {
     
     GLFWwindow *window;
 } SHIZGraphicsContext;
+
+typedef struct {
+    GLuint program;
+    GLuint vbo;
+    GLuint vao;
+} SHIZRenderData;
+
+typedef struct {
+    SHIZPoint position;
+    SHIZColor color;
+} SHIZVertexPositionColor;
 
 #endif // internal_h
