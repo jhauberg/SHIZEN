7d54064593c286ab9f6748260f34694d336eb3b1
Jacob Hauberg Hansen <jacob.hauberg@gmail.com>
2017-09-15 19:06:12 +0200

    Refactor resource loading into separate unit

--- include/SHIZEN/shizen.h
+++ include/SHIZEN/shizen.h
@@ -18,6 +18,7 @@
 #include "draw.h"
 #include "ztime.h"
 #include "input.h"
+#include "loader.h"
 
 /**
  * Provides settings and flags for the creation of the window.
@@ -104,56 +105,5 @@ bool shiz_should_finish(void);
  * @brief Return the display size within the window.
  */
 SHIZSize shiz_get_display_size(void);
-
-/**
- * @brief Load a resource.
- *
- * @return A resource id if the resource was loaded successfully, `0` otherwise
- */
-unsigned int shiz_load(char const * filename);
-/**
- * @brief Unload a resource.
- *
- * @return `true` if the resource was unloaded successfully, `false` otherwise
- */
-bool shiz_unload(unsigned int resource_id);
-
-SHIZSprite shiz_load_sprite(char const * filename);
-SHIZSprite shiz_load_sprite_src(char const * filename,
-                                SHIZRect source);
-
-SHIZSprite shiz_get_sprite(unsigned int resource_id);
-SHIZSprite shiz_get_sprite_src(unsigned int resource_id,
-                               SHIZRect source);
-
-SHIZSpriteSheet shiz_load_sprite_sheet(char const * filename,
-                                       SHIZSize sprite_size);
-SHIZSpriteSheet shiz_load_sprite_sheet_src(char const * filename,
-                                           SHIZSize sprite_size,
-                                           SHIZRect source);
-
-SHIZSpriteSheet shiz_get_sprite_sheet(SHIZSprite resource,
-                                      SHIZSize sprite_size);
-SHIZSpriteSheet shiz_get_sprite_sheet_src(SHIZSprite resource,
-                                          SHIZSize sprite_size,
-                                          SHIZRect source);
-
-SHIZSprite shiz_get_sprite_index(SHIZSpriteSheet spritesheet,
-                                 unsigned int index);
-SHIZSprite shiz_get_sprite_colrow(SHIZSpriteSheet spritesheet,
-                                  unsigned int column,
-                                  unsigned int row);
-
-SHIZSpriteFont shiz_load_sprite_font(char const * filename,
-                                     SHIZSize character);
-SHIZSpriteFont shiz_load_sprite_font_ex(char const * filename,
-                                        SHIZSize character,
-                                        SHIZSpriteFontTable table);
-
-SHIZSpriteFont shiz_get_sprite_font(SHIZSprite sprite,
-                                    SHIZSize character);
-SHIZSpriteFont shiz_get_sprite_font_ex(SHIZSprite sprite,
-                                       SHIZSize character,
-                                       SHIZSpriteFontTable table);
 
 #endif // shizen_h
