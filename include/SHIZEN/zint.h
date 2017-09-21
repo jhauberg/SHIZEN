57c89f982987bd5dc540c3ca2407f6d735aeac6d
Jacob Hauberg Hansen <jacob.hauberg@gmail.com>
2017-09-21 17:22:27 +0200

    Add standard integer/floating point types
    These help make variable intention clearer and declarations shorter.

---
+++ include/SHIZEN/zint.h
@@ -0,0 +1,30 @@
+////
+//    __|  |  | _ _| __  /  __|   \ |
+//  \__ \  __ |   |     /   _|   .  |
+//  ____/ _| _| ___| ____| ___| _|\_|
+//
+// Copyright (c) 2017 Jacob Hauberg Hansen
+//
+// This library is free software; you can redistribute and modify it
+// under the terms of the MIT license. See LICENSE for details.
+//
+
+#ifndef zint_h
+#define zint_h
+
+#include <stdint.h>
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+
+typedef float f32;
+typedef double f64;
+
+#endif /* zint_h */
